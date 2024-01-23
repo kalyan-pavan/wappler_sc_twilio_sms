1ed1ce6f29726a3a03eb4d1ae44e4d2e34ffcd4e
pavan.kalyan <pavan.kalyan@cdmx.in>
2024-01-23 12:54:35 +0530

    Added error handling

--- twilio.js
+++ twilio.js
@@ -3,15 +3,29 @@ exports.twilio_send_sms = async function (options) {
     const accountSid = this.parseRequired(options.account_sid, 'string', 'Account SID is required');
     const authToken = this.parseRequired(options.auth_token, 'string', 'Auth Token is required');
 
+    const messageBody = this.parse(options.message_body);
+    const senderMobile = this.parseRequired(options.sender_mobile, 'string', 'Sender Mobile is required');
+    const recipientMobile = this.parse(options.recipient_mobile) || '';
+
+    // Check if mobile number or message body is empty
+    if (!senderMobile|| !messageBody) {
+        return Promise.reject('Sender Mobile and Message Body cannot be empty');
+    }
+
     const client = require('twilio')(accountSid, authToken);
 
-    client.messages
+    return client.messages
         .create({
-            body: this.parse(options.message_body) || 'No body defined',
-            from: this.parseRequired(options.sender_mobile, 'string', 'Sender Mobile is required'),
-            to: this.parse(options.recipient_mobile) || '',
+            body: messageBody,
+            from: senderMobile,
+            to: recipientMobile,
         })
-        .then(message => console.log(message.sid))
-        .catch(err => console.error(`Error sending message: ${err}`));
-
+        .then(message => {
+            console.log(message.sid);
+            return Promise.resolve(message.sid);
+        })
+        .catch(err => {
+            console.error(`Error sending message: ${err}`);
+            return Promise.reject(`Error sending message: ${err}`);
+        });
 }
